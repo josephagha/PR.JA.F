644f0461115a5e8cf473ca9bce5e0e5ba97bfb74
Joseph Agha <8451229+josephagha@users.noreply.github.com>
2023-03-05 14:20:20 +0100

    Add Router

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,19 @@
 <script>
+  import { Router, Link, Route } from "svelte-navigator";
+  import Home from "./pages/Home.svelte";
+  import NotFound from "./pages/NotFound.svelte";
 </script>
 
-<main>hello world</main>
-
-<style>
-</style>
+<Router>
+  <nav>
+    <Link to="/">Home</Link>
+  </nav>
+  <div>
+    <Route path="/">
+      <Home />
+    </Route>
+    <Route path="*">
+      <NotFound />
+    </Route>
+  </div>
+</Router>
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,25 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import sveltePreprocess from 'svelte-preprocess';
 import path from 'path';
+import { resolve } from "path";
+
+const pathAliases = {
+  "/@routes/": resolve(__dirname, "routes"),
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte({
+      preprocess: sveltePreprocess(pathAliases),
+    }),
+  ],
   resolve: {
     alias: {
-      '@': path.resolve('/src'),
+      "@": path.resolve(__dirname, "src"),
     },
-  }
+  },
 })
+
+
